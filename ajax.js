d68857c20afc376188c23e3c0ace141ac99107ae
nanda <nandabaley36@gmail.com>
2025-12-02 16:14:19 +0700

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -2,11 +2,10 @@ const form = document.getElementById("contactForm");
 const successMsg = document.getElementById("successMsg");
 
 form.addEventListener("submit", function(e) {
-    e.preventDefault(); // mencegah reload
+    e.preventDefault();
 
-    // tampilkan pesan sukses
     successMsg.classList.remove("hidden");
 
-    // reset form
     form.reset();
 });
+
